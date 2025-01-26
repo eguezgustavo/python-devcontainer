430df13154966917561153214bdbe05af65333cb
Gustavo Eguez <eguezgustavo@gmail.com>
2025-01-26 04:13:28 +0000

    improves post installation scripts

--- .devcontainer/post_installation.sh
+++ .devcontainer/post_installation.sh
@@ -2,3 +2,6 @@
 
 git config --global user.email ${GITHUB_EMAIL}
 git config --global user.name ${GITHUB_NAME}
+
+/home/developer/.local/bin/poetry config virtualenvs.in-project true
+/home/developer/.local/bin/poetry init
